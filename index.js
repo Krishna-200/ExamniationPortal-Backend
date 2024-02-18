41302969a5549405b90210fed2105600da580ea2
krishna-200 <20mh1a4242@acoe.edu.in>
2024-02-18 23:23:21 +0530

    error correction

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "https://examination-portal-six.vercel.app/",
+    origin: "https://examination-portal-six.vercel.app",
   })
 );
 app.use(cookieParser());
