632b04bf59b714fb609b8c7cf31f1e125ff8e6ba
krishna-200 <20mh1a4242@acoe.edu.in>
2024-02-18 23:16:27 +0530

    frontend link update

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://examination-portal-six.vercel.app/",
   })
 );
 app.use(cookieParser());
