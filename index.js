62e986ac92b528edc1f7074da59efdab79bd073f
krishna-200 <20mh1a4242@acoe.edu.in>
2024-03-18 22:01:24 +0530

    CORS error correction

--- index.js
+++ index.js
@@ -19,6 +19,16 @@ const path = require("path");
 
 const app = express();
 const bcryptSalt = bcrypt.genSaltSync(10);
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(express.json());
 app.use(
   cors({
