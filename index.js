da9d633b1170f8910baba8a7bfebc6ed75bc5a35
krishna-200 <20mh1a4242@acoe.edu.in>
2024-03-18 22:16:31 +0530

    CORS link update

--- index.js
+++ index.js
@@ -21,11 +21,9 @@ const app = express();
 const bcryptSalt = bcrypt.genSaltSync(10);
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.setHeader("Access-Control-Allow-Origin", "https://examify.netlify.app/");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
